c6ae644bd154bdca033277c0564c8cad8c686404
ChadPerkins <web3.engineering@gmail.com>
2022-03-31 18:11:00 -0400

    added a new project to my list

--- src/components/ProjectCard/ProjectCardData.jsx
+++ src/components/ProjectCard/ProjectCardData.jsx
@@ -1,13 +1,22 @@
+import CodeSnippet from "../../imgs/code-snippets.png";
 import Portfolio from "../../imgs/portfolio.png";
-import YelpCamp1 from "../../imgs/yelpcamp1.png";
-import YtClone1 from "../../imgs/ytclone1.png";
+import YelpCamp from "../../imgs/yelpcamp.png";
+import YtClone from "../../imgs/ytclone.png";
 
 export const projects = [
+    {
+        title: "Code-Snippets",
+        description:
+            "A Codepen style clone created with React. This website has fully functional HTML, CSS and Javascript terminals with a window that updates as you type. This project has local stroage to save your progress",
+        img: CodeSnippet,
+        github: "https://github.com/ChadPerkins/Code-Snippet",
+        url: "https://comfy-dragon-90f89e.netlify.app/",
+    },
     {
         title: "YelpCamp",
         description:
             "A yelp clone dedicated to camp sites. This project uses NodeJS, MongoDB, Express with vanilla JS. This project has full CRUD functionality and user verification.",
-        img: YelpCamp1,
+        img: YelpCamp,
         github: "https://github.com/ChadPerkins/Yelp-Camp",
         url: "https://enigmatic-cliffs-90841.herokuapp.com/",
     },
@@ -15,7 +24,7 @@ export const projects = [
         title: "YouTube-Clone",
         description:
             "This is a project connecting to the Youtube API. It is used for searching and displaying videos as well as comments from the parent video. It is built using the React framework.",
-        img: YtClone1,
+        img: YtClone,
         github: "https://github.com/ChadPerkins/Youtube-Clone",
         url: "https://youtube-clone-eight-steel.vercel.app/",
     },
